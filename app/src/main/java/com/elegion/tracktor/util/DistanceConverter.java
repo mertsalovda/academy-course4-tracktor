c9b228c3e862754b93b5a2afdb8c6c67b6bd3b59
mertsalovda <korranhorn009@gmail.com>
2020-08-07 03:39:45 +0300

    Added DistanceConverterTest. Refactored DistanceConverter

--- app/src/main/java/com/elegion/tracktor/util/DistanceConverter.java
+++ app/src/main/java/com/elegion/tracktor/util/DistanceConverter.java
@@ -7,24 +7,18 @@ public class DistanceConverter {
     public static final int METER = 3;
     public static final int FOOT = 4;
 
-    public static String formatDistance(Double meter, int unit) {
-        String result = "";
+    public static String formatDistance(double meter, int unit) {
         switch (unit) {
             case KILOMETER:
-                result = MathUtils.round((meter / 1000), 2) + " км";
-                break;
+                return MathUtils.round((meter / 1000), 2) + " км";
             case MILES:
-                result = MathUtils.round((meter * 0.000621371), 2) + " миль";
-                break;
+                return MathUtils.round((meter * 0.000621371), 2) + " миль";
             case METER:
-                result = MathUtils.round(meter, 2) + " м";
-                break;
+                return MathUtils.round(meter, 2) + " м";
             case FOOT:
-                result = MathUtils.round(meter * 3.281, 2) + " футов";
-                break;
+                return MathUtils.round(meter * 3.281, 2) + " футов";
             default:
                 throw new IllegalStateException("Неизвестная единица измерения длины: " + unit);
         }
-        return result;
     }
 }
--- app/src/test/java/com/elegion/tracktor/util/DistanceConverterTest.java
+++ app/src/test/java/com/elegion/tracktor/util/DistanceConverterTest.java
@@ -1,11 +1,41 @@
 package com.elegion.tracktor.util;
 
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class DistanceConverterTest {
 
     @Test
-    public void formatDistance() {
+    public void formatDistanceKilometer() {
+        assertEquals("1.0 км", DistanceConverter.formatDistance(1000.0, DistanceConverter.KILOMETER));
+        assertNotEquals("1.0 км", DistanceConverter.formatDistance(2000.0, DistanceConverter.KILOMETER));
+    }
+
+    @Test
+    public void formatDistanceKilometer2() {
+        assertEquals("2.0 км", DistanceConverter.formatDistance(2000.0, DistanceConverter.KILOMETER));
+        assertNotEquals("2.0 км", DistanceConverter.formatDistance(1000.0, DistanceConverter.KILOMETER));
+    }
+    @Test
+    public void formatDistanceMeter() {
+        assertEquals("1000.0 м", DistanceConverter.formatDistance(1000.0, DistanceConverter.METER));
+        assertNotEquals("1000.0 м", DistanceConverter.formatDistance(2000.0, DistanceConverter.METER));
+    }
+    @Test
+    public void formatDistanceMiles() {
+        assertEquals("0.62 миль", DistanceConverter.formatDistance(1000.0, DistanceConverter.MILES));
+        assertNotEquals("0.62 миль", DistanceConverter.formatDistance(2000.0, DistanceConverter.MILES));
+    }
+
+    @Test
+    public void formatDistanceFoot() {
+        assertEquals("3281.0 футов", DistanceConverter.formatDistance(1000.0, DistanceConverter.FOOT));
+        assertNotEquals("3281.0 футов", DistanceConverter.formatDistance(2000.0, DistanceConverter.FOOT));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void formatDistanceException(){
+        DistanceConverter.formatDistance(1000.0, -1);
     }
 }
